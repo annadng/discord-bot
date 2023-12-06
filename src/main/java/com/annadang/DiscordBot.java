7689b7ca37a2af3f6a940573aed08a403e4e7d45
Anna Dang <annadang2001@gmail.com>
2023-12-06 20:59:46 +1100

    Hide API token via ignored file

--- src/main/java/com/annadang/DiscordBot.java
+++ src/main/java/com/annadang/DiscordBot.java
@@ -4,13 +4,30 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
+import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.io.InputStream;
+
 public class DiscordBot {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
 
-        JDA bot = JDABuilder.createDefault("MTE4MTU1NTU0MTc1MTkwMjI3MA.GZKf-8.y3-YtyVIxheL5zxy3Sf6_K0togmZfRRA2ESdJc")
-                .setActivity(Activity.playing(""))
-                .build();
+        Properties properties = new Properties();
+
+        try {
+            InputStream input = new FileInputStream("config.properties");
+            properties.load(input);
+            String apiKey = properties.getProperty("apiKey");
+
+            System.out.println(apiKey);
+
+            JDA bot = JDABuilder.createDefault(apiKey)
+                    .setActivity(Activity.playing("activity"))
+                    .build();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
