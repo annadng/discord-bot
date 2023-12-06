f597d38c410a6792721031fdbd5897f5f658b15a
Anna Dang <annadang2001@gmail.com>
2023-12-06 21:10:49 +1100

    Remove throwing of LoginException

--- src/main/java/com/annadang/DiscordBot.java
+++ src/main/java/com/annadang/DiscordBot.java
@@ -4,14 +4,13 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
-import javax.security.auth.login.LoginException;
 import java.io.FileInputStream;
 import java.util.Properties;
 import java.io.InputStream;
 
 public class DiscordBot {
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) {
 
         Properties properties = new Properties();
 
