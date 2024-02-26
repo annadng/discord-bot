2dc131fb4745258cf98df44d8b29ee8c883439c3
Anna Dang <annadang2001@gmail.com>
2024-02-26 13:13:56 +1100

    Add searchYouTube function

--- src/main/java/com/annadang/DiscordEventListener.java
+++ src/main/java/com/annadang/DiscordEventListener.java
@@ -6,22 +6,29 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.Video;
-import net.dv8tion.jda.api.entities.Message;
+import com.google.api.services.youtube.model.SearchListResponse;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 public class DiscordEventListener extends ListenerAdapter {
 
     // holds an instance of the DiscordBot class
     public DiscordBot bot;
+    // holds an instance of YouTube
+    private YouTube youTube = null;
 
     // constructor to assign an instance of the DiscordBot class to bot, constructs a DiscordEventListener object
     public DiscordEventListener(DiscordBot bot) {
 
         this.bot = bot;
-        YouTube youTube = null;
 
+        // creates an instance of YouTube
         try {
-            youTube = new YouTube.Builder(new NetHttpTransport(), new GsonFactory(), null)
+            this.youTube = new YouTube.Builder(new NetHttpTransport(), new GsonFactory(), null)
                     .setApplicationName("Discord Bot").build();
 
         } catch(Exception e) {
@@ -36,10 +43,35 @@ public class DiscordEventListener extends ListenerAdapter {
         if(!event.getAuthor().isBot()) {
             String message = event.getMessage().getContentRaw();
             if(message.startsWith("!play")) {
-                String query = message.substring("!play".length()).trim();
+                String searchQuery = message.substring("!play".length()).trim();
+                searchYouTube(searchQuery);
             }
         }
 
+    }
+
+    public void searchYouTube(String searchQuery) {
+
+        Properties properties = new Properties();
+
+        try {
+            // retrieve YouTube data API key
+            InputStream input = new FileInputStream("config.properties");
+            properties.load(input);
+            String youTubeKey = properties.getProperty("youTubeKey");
+
+            // perform search using YouTube data API
+            YouTube.Search.List searchResults = youTube.search()
+                    .list("id,snippet")
+                    .setQ(searchQuery)
+                    .setType("video")
+                    .setKey(youTubeKey)
+                    .execute()
+                    .getItems();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
